3e3d9949d3ffe3f50a6035be2b1a211511b5b8a5
Nicolas <eighthsatellite@gmail.com>
2017-07-14 18:38:39 +0300

    added form input component

---
+++ src/components/FormInput/FormInput.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react'
+import { TextInput, View } from 'react-native'
+import styles from './FormInputStyles'
+
+
+export default class FormInput extends PureComponent {
+  render() {
+    const { input, meta, ...inputProps } = this.props
+
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          {...inputProps}
+          onChangeText={input.onChange}
+          onBlur={input.onBlur}
+          onFocus={input.onFocus}
+          value={input.value}
+          style={styles.input}
+          placeholderTextColor="rgba(255, 255, 255, 0.5)"
+        />
+      </View>
+    )
+  }
+}
