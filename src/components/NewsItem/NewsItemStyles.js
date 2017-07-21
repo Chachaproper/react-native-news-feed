195b0922eb8898dddd836a369488820877637641
Nicolas <eighthsatellite@gmail.com>
2017-07-21 12:53:28 +0300

    fix shadows in ios

--- src/components/NewsItem/NewsItemStyles.js
+++ src/components/NewsItem/NewsItemStyles.js
@@ -13,8 +13,8 @@ export default StyleSheet.create({
     marginTop: 5,
     shadowColor: '#000',
     backgroundColor: 'white',
-    shadowOffset: { width: 0, height: 50 },
-    shadowOpacity: 0.8,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
     shadowRadius: 2,
     elevation: 3,
     borderRadius: 2
